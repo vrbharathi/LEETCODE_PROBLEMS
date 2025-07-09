bc73bf3c4168b9f8d23b00fa502981154cd04e36
vrbharathi <vrbharathi192@gmail.com>
2025-07-09 03:22:00 +0000

    Sync LeetCode submission Runtime - 5 ms (61.42%), Memory - 43.2 MB (99.97%)

---
+++ 0242-valid-anagram/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+            freq[t.charAt(i)]--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) return false;
+        }
+
+        return true;
+    }
+}
+
