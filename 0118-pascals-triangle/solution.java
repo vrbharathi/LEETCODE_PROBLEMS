d3736728d9f4582a3eb7a34e579decb075657a44
vrbharathi <vrbharathi192@gmail.com>
2025-07-23 02:50:08 +0000

    Sync LeetCode submission Runtime - 1 ms (94.24%), Memory - 42.2 MB (99.96%)

---
+++ 0118-pascals-triangle/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int val = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
+                    row.add(val);
+                }
+            }
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);     
+        int numRows = sc.nextInt();               
+
+        Solution obj = new Solution();            
+        List<List<Integer>> result = obj.generate(numRows); 
+        for (List<Integer> row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
