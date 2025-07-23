333c9c3b7a28d2d4b5de93ff712f3c4c6af86b09
vrbharathi <vrbharathi192@gmail.com>
2025-07-23 02:07:50 +0000

    Sync LeetCode submission Runtime - 2 ms (19.25%), Memory - 41.8 MB (99.98%)

---
+++ 0075-sort-colors/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static void sortColors(int[] nums) {
+         for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+            }
+        }
+        }
+    }
+    public static void main(String[] args){
+        int[] nums={1,0,2,1,0};
+        sortColors(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
