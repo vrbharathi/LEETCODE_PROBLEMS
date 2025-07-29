a1ce3fd16c934acd77b38080408aa3748db7a75c
vrbharathi <vrbharathi192@gmail.com>
2025-07-29 10:54:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.6 MB (99.93%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;      // Pointer for nums1
-        int j = n - 1;      // Pointer for nums2
-        int k = m + n - 1;  // Pointer for final position in nums1
+        int i = m - 1;    
+        int j = n - 1;      
+        int k = m + n - 1;  
 
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
